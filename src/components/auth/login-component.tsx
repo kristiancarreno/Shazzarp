2d11b4efe82f9b512cc68f5cd63763a63c78773b
Kristian Carreno <kristian.cc@avangenio.com>
2024-07-14 19:01:45 -0400

    feat(style): change colors

--- src/components/auth/login-component.tsx
+++ src/components/auth/login-component.tsx
@@ -13,36 +13,36 @@ export default function Login() {
     signIn('github')
   }
   return (
-    <div className='flex min-h-[100dvh] items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8'>
-      <Card className='w-full max-w-md'>
+    <div className='flex min-h-[100dvh] items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8 '>
+      <Card className='w-full max-w-md bg-lightBlack border-hoveredButton'>
         <CardHeader>
           {status === 'unauthenticated' ? (
             <>
               <div className='flex items-center gap-4'>
-                <GithubIcon />
-                <CardTitle className='text-2xl'>Bienvenido de vuelta</CardTitle>
+                <GithubIcon color='white' />
+                <CardTitle className='text-2xl text-zinc-300'>Bienvenido de vuelta</CardTitle>
               </div>
-              <CardDescription>Entra con tu cuenta de GitHub.</CardDescription>
+              <CardDescription className='text-zinc-300'>Entra con tu cuenta de GitHub.</CardDescription>
             </>
           ) : (
             <>
               <div className='flex items-center gap-4'>
-                <CheckCheck />
-                <CardTitle className='text-2xl'>Haz iniciado sesión correctamente</CardTitle>
+                <CheckCheck color='white' />
+                <CardTitle className='text-2xl text-zinc-300'>Haz iniciado sesión correctamente</CardTitle>
               </div>
-              <CardDescription>Tus colegas te esperan.</CardDescription>
+              <CardDescription className='text-zinc-300'>Tus colegas te esperan.</CardDescription>
             </>
           )}
         </CardHeader>
         <CardContent className='space-y-4'></CardContent>
         <CardFooter>
           {status === 'unauthenticated' ? (
-            <Button onClick={singIn} className='w-full bg-[#0077b6] hover:bg-[#005a8c] text-white'>
+            <Button onClick={singIn} className='w-full bg-[#358841] hover:bg-[#598A60] text-white'>
               Inicia Session
             </Button>
           ) : (
             <Link className='w-full' href={'/chat-view'}>
-              <Button className='w-full bg-[#0077b6] hover:bg-[#005a8c] text-white'>Comienza a chatear</Button>
+              <Button className='w-full bg-[#358841] hover:bg-[#598A60] text-white'>Comienza a chatear</Button>
             </Link>
           )}
         </CardFooter>
